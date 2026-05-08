4fcf0b35493ea7b49c95f9f8c441576cc8c98256
SebasSis23 <sebastianfloresfuertes@gmail.com>
2026-05-08 09:13:12 -0400

    Primer commit: Estructura limpia con Java 17 y paquetes corregidos

---
+++ src/test/java/Ctrl_c_Ctrl_v/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Ctrl_c_Ctrl_v.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
