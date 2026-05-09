bebd39c488f4ed912875b1bb3b40eb2258ac8789
SebasSis23 <sebastianfloresfuertes@gmail.com>
2026-05-09 12:13:14 -0400

    Revert "Añadiendo Oficina"

--- src/main/java/com/example/sprintbot/repository/OrguserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sprintbot.repository;
-
-import com.example.sprintbot.entity.Orguser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrguserRepository extends JpaRepository<Orguser, String> {
-
-}
--- src/main/java/com/example/unidadadmin/DemoApplication.java
+++ src/main/java/com/example/unidadadmin/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.unidadadmin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoApplication {
 
-	private DemoApplication() {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
