81add5c151878e514d83f574d80a297fb9f5c552
SebasSis23 <sebastianfloresfuertes@gmail.com>
2026-05-08 22:34:37 -0400

    Tabla Oficina creada y añadida para el trabajo grupal

---
+++ src/main/java/Ctrl_c_Ctrl_v/demo/Oficina.java
@@ -0,0 +1,16 @@
+package Ctrl_c_Ctrl_v.demo;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class Oficina {
+    private String entidad;    
+    private String unidad;     
+    private int codofic;       
+    private String nomofic;    
+    private String observ;     
+    private Date feult;        
+    private String usuar;      
+    private int api_estado;    
+}
