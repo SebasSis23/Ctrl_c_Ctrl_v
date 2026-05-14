be778c27e17675e1639f37bf7d3d9a5e4bc8c617
TrinidadRamirez-ship-it <munozashelem@gmail.com>
2026-05-13 23:14:19 -0400

    Correccion modelo Orguser

--- src/main/java/Ctrl_c_Ctrl_v/demo/Model/Orguser.java
+++ src/main/java/Ctrl_c_Ctrl_v/demo/Model/Orguser.java
@@ -1,28 +1,16 @@
 package Ctrl_c_Ctrl_v.demo.Model;
 
-import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.Date;
 
-@Entity
-@Table(name = "orguser")
 @Data
 public class Orguser {
 
-    @Id
     private String id;
-
     private String tipo;
-
     private String nombre;
-
     private Boolean soloLectura;
-
     private Integer ckval;
-
-    @Column(columnDefinition = "TEXT")
     private String datos;
-
     private Date fechaActualizacion;
 }
