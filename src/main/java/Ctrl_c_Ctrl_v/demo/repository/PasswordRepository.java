507bd660f1c1616080a1002ae189931b1358925d
Arik2-p <39657740a@gmail.com>
2026-05-20 23:32:16 -0400

    Mi Base de datos - Ariel Rodrigo Montesinos Ventura

---
+++ src/main/java/Ctrl_c_Ctrl_v/demo/repository/PasswordRepository.java
@@ -0,0 +1,12 @@
+package Ctrl_c_Ctrl_v.demo.repository;
+
+import Ctrl_c_Ctrl_v.demo.Entity.PasswordEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordRepository
+        extends JpaRepository<PasswordEntity, Integer> {
+
+}
---
+++ src/main/java/Ctrl_c_Ctrl_v/demo/service/PasswordService.java
@@ -0,0 +1,73 @@
+package Ctrl_c_Ctrl_v.demo.service;
+
+import Ctrl_c_Ctrl_v.demo.Entity.PasswordEntity;
+import Ctrl_c_Ctrl_v.demo.repository.PasswordRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PasswordService {
+
+    @Autowired
+    private PasswordRepository repository;
+
+    // LISTAR
+    public List<PasswordEntity> obtenerTodos() {
+
+        return repository.findAll();
+    }
+
+    // BUSCAR POR ID
+    public PasswordEntity obtenerPorId(int id) {
+
+        Optional<PasswordEntity> dato = repository.findById(id);
+
+        return dato.orElse(null);
+    }
+
+    // GUARDAR
+    public PasswordEntity guardar(PasswordEntity password) {
+
+        return repository.save(password);
+    }
+
+    // ACTUALIZAR
+    public PasswordEntity actualizar(int id, PasswordEntity nuevo) {
+
+        PasswordEntity existente = obtenerPorId(id);
+
+        if (existente != null) {
+
+            existente.setCompleto(nuevo.getCompleto());
+            existente.setDes(nuevo.getDes());
+            existente.setPsw(nuevo.getPsw());
+            existente.setTipo(nuevo.getTipo());
+            existente.setGrupo(nuevo.getGrupo());
+            existente.setFeult(nuevo.getFeult());
+            existente.setUsuar(nuevo.getUsuar());
+
+            return repository.save(existente);
+        }
+
+        return null;
+    }
+
+    // ELIMINAR
+    public String eliminar(int id) {
+
+        PasswordEntity existente = obtenerPorId(id);
+
+        if (existente != null) {
+
+            repository.deleteById(id);
+
+            return "Registro eliminado";
+        }
+
+        return "No encontrado";
+    }
+}
